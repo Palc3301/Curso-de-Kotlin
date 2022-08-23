6e98b53e6b5325a4c13049b518f7df6f5d0242b2
Pedro Arruda <pedroarruda8@live.com>
2022-08-23 19:33:32 -0300

    First day Kotlin

---
+++ src/main/kotlin/PrimeiroCodigo.kt
@@ -0,0 +1,12 @@
+/*fun main(args: Array<String>) {
+    println("Hello World!")
+}*/
+fun main() {
+    println(max(10, 5))
+}
+// Corpo de bloco
+fun max(a: Int, b:Int): Int {
+    return if (a > b) a else  b
+}
+// Corpo de expressao
+fun max1(a: Int, b: Int): Int = if (a > b) a else  b
