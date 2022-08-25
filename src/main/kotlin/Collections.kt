d94271e8b5afd83953b347bf21cba2dfbe9c0a43
Pedro Arruda <pedroarruda8@live.com>
2022-08-25 18:28:46 -0300

    Collections

---
+++ src/main/kotlin/Collections.kt
@@ -0,0 +1,14 @@
+fun main () {
+
+    val frutas1 =ArrayList<String>()
+    frutas1.add("Maça")
+    frutas1.add("Banana")
+    frutas1.add("Morango")
+    println(frutas1)
+
+    val frutas2 = arrayListOf<String>("Maça", "Banana", "Morango")
+
+    println(HashSet::class.java)
+
+    println(frutas2.last())
+}
